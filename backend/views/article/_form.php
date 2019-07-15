4a4f1874599e01b8b192f42bea46ff13a5829d83
zxl976557204 <976557204@qq.com>
2019-07-15 17:51:13 +0800

    添加上传二维码控件

--- backend/views/article/_form.php
+++ backend/views/article/_form.php
@@ -15,6 +15,7 @@ use common\modules\attachment\widgets\SingleWidget;
 /* @var $form backend\widgets\ActiveForm */
 ?>
     <?php $form = ActiveForm::begin([
+        'options' => ['enctype' => 'multipart/form-data'],
         'enableClientValidation' => false,
         'enableAjaxValidation' => true
     ]); ?>
@@ -27,9 +28,9 @@ use common\modules\attachment\widgets\SingleWidget;
                 <div class="tab-pane active" id="tab_1">
 
                     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
-
-                    <?= $form->field($model, 'category_id')->dropDownList(Category::getDropDownList(Tree::build(Category::lists($model->module)))) ?>
-
+                     <?= $form->field($model, 'category_id')->dropDownList(Category::getDropDownList(Tree::build(Category::lists($model->module)))) ?>
+                    <?= $form->field($model, 'cover')->widget(SingleWidget::className()) ?>   
+                    <?= $form->field($model, 'qr_code')->widget(SingleWidget::className()) ?>
                     <?= $form->field($model, 'description')->textarea()?>
 
                     <?php foreach ($moduleModel->formAttributes() as $attribute): ?>
@@ -51,8 +52,10 @@ use common\modules\attachment\widgets\SingleWidget;
                             'pluginOptions' => ['autoclose' => true]
                         ]
                     ) ?>
-                    <?= $form->field($model, 'cover')->widget(SingleWidget::className()) ?>
+                 
 
+                 
+                  
                     <?= $form->field($model, 'commentEnabled')->checkbox(['label' => '开启评论']) ?>
 
                     <?= $form->field($model, 'is_top')->checkbox() ?>
