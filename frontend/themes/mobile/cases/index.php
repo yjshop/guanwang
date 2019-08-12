1f31982aaef151f8af924d3ec527ecf6e83ee851
dayidecha <451372718@qq.com>
2019-08-12 18:28:27 +0800

    39

--- frontend/themes/mobile/cases/index.php
+++ frontend/themes/mobile/cases/index.php
@@ -126,7 +126,7 @@ $this->registerJs(<<<JS
     //其他信息
     var message;
     //滑动锁定
-    var flag=1;
+    var flag=true;
     //已经进行第一次请求
     var flag2=false;
 
@@ -154,6 +154,7 @@ $this->registerJs(<<<JS
       })
        imgLoad(imgData);
 
+
   }).fail(function (xhr, status) {
 
   }).always(function () {
@@ -162,70 +163,47 @@ $this->registerJs(<<<JS
 
 
   //滑动加载
-  $(window).scroll(function(){
-
-
-            if(((flag==1)&&$(document).scrollTop()+100)>=$(document).height()-$(window).height()-$('footer').height()){
-
-               flag--;
-
-
-              // 是否还有新的图片没加载进来
-      if(flag2&&message.currentPage<message.pageCount){
-        imgData=[];
-      $.ajax(next.href, {
-    dataType: 'json',
-     beforeSend:function(){
-      
-      $("#loading").html('加载中...');
-      
+ $(window).scroll(function() {
+    var condition1 = $(document).scrollTop() + 100 >= $(document).height() - $(window).height() - $('footer').height();
+    if (flag === true && condition1) {
+        //是否可以请求ajax
+        
+        // 是否还有新的图片没加载进来
+        if (flag2 && message.currentPage < message.pageCount) {
+            imgData = [];
+            $.ajax(next.href, {
+                dataType: 'json',
+                beforeSend: function() {
+                  flag = false;
+                  $("#loading").html('加载中...');
+                }
+            }).done(function(data) {
+                $("#loading").empty();
+                next = data._links.next;
+                message = data._meta;
+                $.each(data.items, function(index, value) {
+                    imgData.push({
+                        cover: value.cover,
+                        id: value.id
+                    });
+                })
+                imgLoad(imgData);
+            }).fail(function(xhr, status) {}).always(function() {});
+        } else {
+            $("#loading").html('没有更多案例了 ^_^');
+        }
     }
-  }).done(function (data) {
-
-      $("#loading").empty();
-      next=data._links.next;
-      message=data._meta;
-
-      $.each(data.items,function(index,value){
-
-        imgData.push({cover:value.cover,id:value.id});
-      })
-       imgLoad(imgData);
-
-  }).fail(function (xhr, status) {
-
-  }).always(function () {
-
-  });
-}else{
-   $("#loading").html('没有更多案例了 ^_^');
-}
-
-
-
-            }
-
-         });
-
-      //将图片输出到页面，data为存储图片路径的数组
-function imgLoad(data){
-
-            //滑动时只加载一次
-            flag=0;
-            console.log(data);
-            for(var i=0;i<data.length;i++){
-
-              itemss=$('<li class="anli-box"><a href="/cases/view?id='+data[i].id+'"><img src="'+data[i].cover+'"></a></li>');
-
-              $('.anli-m').append(itemss).masonry( 'appended',itemss);
-
-            }
-          $('.anli-m').imagesLoaded().progress(function(){
-
-          $('.anli-m').masonry('layout');
-         })
-          flag=1;
-
+});
+//将图片输出到页面，data为存储图片路径的数组
+function imgLoad(data) {
+    for (var i = 0; i < data.length; i++) {
+        itemss = $('<li class="anli-box"><a href="/cases/view?id=' + data[i].id + '"><img src="' + data[i].cover + '"></a></li>');
+        $('.anli-m').append(itemss).masonry('appended', itemss);
+    }
+    $('.anli-m').imagesLoaded().progress(function() {
+        $('.anli-m').masonry('layout');
+    })
+    flag = true;
 }
 }
 
