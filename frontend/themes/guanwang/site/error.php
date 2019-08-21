598c31b94bf93a6531027d80f6c8d44138c4304b
dayidecha <451372718@qq.com>
2019-08-21 10:47:43 +0800

    44

--- frontend/themes/guanwang/site/error.php
+++ frontend/themes/guanwang/site/error.php
@@ -14,8 +14,11 @@ $this->title = $name;
 <div class="site-error">
 
 
-    <div class="jumbotron" style="min-height: 600px; background-color: white;">
-  <h1>404  页面找不到了</h1>
+    <div class="jumbotron" style="background-color: white; ">
+  <h1 class="center-block">404 NOT FOUND</h1>
+  <h2  class="" style="display: inline-block;">您访问的页面不见了，去其他页面看看吧</h2>
+  <p style="margin-top: 30px;"><a class="btn btn-default btn-lg" href="/" role="button" style="margin-right: 20px;">返回首页</a>
+    <a class="btn btn-default btn-lg" href="#" role="button" onclick="javascript:window.history.back(-1);">返回前页</a></p>
   <!-- <h2>404</h2> -->
  <!--  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p> -->
 </div>
@@ -24,7 +27,7 @@ $this->title = $name;
 
 
   
-    <div class="error-search">
+   <!--  <div class="error-search">
         <form action="<?= url(['/search']) ?>" method="get">
             <div class="input-group">
                 <input type="text" class="form-control" name="q" placeholder="全站搜索">
@@ -35,7 +38,7 @@ $this->title = $name;
                 </span>
             </div>
         </form>
-    </div>
+    </div> -->
 
 
 </div>
--- frontend/themes/mobile/site/error.php
+++ frontend/themes/mobile/site/error.php
@@ -11,7 +11,14 @@ $this->title = $name;
 ?>
 <div class="site-error">
 
-    <h1>页面找不到了</h1>
+     <div class="jumbotron" style="background-color: white; ">
+  <h1 class="center-block">404 NOT FOUND</h1>
+  <h2  class="" style="display: inline-block;">您访问的页面不见了，去其他页面看看吧</h2>
+  <p style="margin-top: 30px;"><a class="btn btn-default btn-lg" href="/" role="button" style="margin-right: 20px;">返回首页</a>
+    <a class="btn btn-default btn-lg" href="#" role="button" onclick="javascript:window.history.back(-1);">返回前页</a></p>
+  <!-- <h2>404</h2> -->
+ <!--  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p> -->
+</div>
     <div class="error-search">
         <form action="<?= url(['/search']) ?>" method="get">
             <div class="input-group">
