d04639496ecec40d8ab9af0e58cb8519fe4a8616
‘wsyone <774294449@qq.com>
2019-08-23 17:57:57 +0800

    修改qq 电话配置

--- frontend/themes/mobile/layouts/_footer.php
+++ frontend/themes/mobile/layouts/_footer.php
@@ -34,8 +34,8 @@ use yii\helpers\Url;
         <img src="/storage/images/tel.png" class="tel-img">
         <div style="display: inline-block;">
          <p class="tit">咨询热线</p> 
-         <p class="tel">0777-2110336</p>
-         <p class="qq tel-hide" onclick="window.location.href='http://wpa.qq.com/msgrd?v=3&uin=1636861793&site=qq&menu=yes'">在线QQ咨询</p>
+         <p class="tel"><?= Yii::$app->config->get('TEL')?></p>
+         <p class="qq tel-hide" onclick="window.location.href='http://wpa.qq.com/msgrd?v=3&uin=<?= Yii::$app->config->get('qq')?>&site=qq&menu=yes'">在线QQ咨询</p>
         </div>
       </div>
     </div>
@@ -77,7 +77,7 @@ use yii\helpers\Url;
     
         <div><img src="<?php echo Url::to('@web/storage/images/phone1.png') ?>" ></div>
        <div><p>服务热线</p>
-        <p>0777-1234567</p>
+        <p><?= Yii::$app->config->get('TEL')?></p>
       </div>
         </a>  
     </li> -->
@@ -121,7 +121,7 @@ use yii\helpers\Url;
     
         <div><img src="<?php echo Url::to('@web/storage/images/phone1.png') ?>" ></div>
        <div><p>服务热线</p>
-        <p>0777-1234567</p>
+        <p><?= Yii::$app->config->get('TEL')?></p>
       </div>
         </a>  
 
